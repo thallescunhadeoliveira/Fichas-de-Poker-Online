98ef2bed6a8980da4c84ee73b9858807d9098db6
Thalles Oliveira <89935401+thallescunhadeoliveira@users.noreply.github.com>
2022-01-27 22:11:08 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-# Este código tem alterações na forma de consulta SQL em relação ao documento application.ipynb
+# Este código tem alterações na forma de consulta SQL em relação ao documento application_pyodbc.ipynb
 # Pode ser rodado no ambiente CS50 IDE, ambiente virtual Linux Ubuntu AWS disponibilizado por Harvard no curso de introdução à ciência de computação CS50
 
 from cs50 import SQL
